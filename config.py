d6962acd1103340bd972bdcee1a02b5fbc879285
gilmniar <132059572+gilmniar@users.noreply.github.com>
2023-06-09 13:35:33 +0500

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-tg_bot_token = '6019660701:AAFKWGzfqNzgx-g3JTTSc4AwHsQPgroygaE'
-headers = {"apikey": "xiJtliQymIzKsCRfLm7plY7jrFUXAEGZ"}
-
-keys = {
-    'доллар': 'USD',
-    'рубль': 'RUB',
-    'евро': 'EUR',
-    'юань': 'CNY'
-}
